cf39cfa1ecda94c1c4b0e51a7d1f889e7bf72fd3
unknown <tongjac@yahoo.com>
2021-01-27 17:07:08 -0800

    Added functionality to posting comments to unique articles

--- app/public/js/articlesHTML.js
+++ app/public/js/articlesHTML.js
@@ -1,7 +1,7 @@
 var articleID;
-//waiting for DOM content to be loaded before running JS
+
+// Waiting for DOM content to be loaded before running JS
 document.addEventListener("DOMContentLoaded", (e) => {
-  // console.log("DOM Loaded");
 
   const articleContainer = document.getElementById("articleContainer");
   const articleTitleList = document.getElementById("articleTitleList");
@@ -10,43 +10,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const searchOnClick = document.getElementById("searchOnClick")
   const itemTitle = document.getElementById("itemTitle")
 
-  
-
   //articles array is getting data from the getArticles function
   let articles = [];
 
-  let testFunc = (e) => {
-    if(e.target.matches(".articleTitle")){
-      let id = parseInt(e.target.getAttribute("data-id")) - 1;
-      renderArticle(id)
-    }
-   
-  }
-
-  //adding titles to search col
-  const searchCol = () => {
-   
-    for (let i = 0; i < articles.length; i++) {
-      // console.log(articles[i].title);
-      let listItem = document.createElement("li");
-      let ArticleTitle = articles[i].title;
-      listItem.classList.add("articleTitle");
-      listItem.setAttribute("data-id", `${articles[i].id}`)
-      listItem.innerHTML = ArticleTitle;
-      articleTitleList.appendChild(listItem);
-
-
-    }
-    // console.log(listItem)
-  };
-  
-  const renderArticle = (id) => {
-    articleID = parseInt(id);
-    let title = articles[id].title
-    let content = articles[id].content
-    htmlTitle.innerHTML = title;
-    articleContent.innerHTML = content;
-  }
   //helper function to get articles
   //the data is then added to the articles array
   //using some sort of witchcraft
@@ -60,17 +26,43 @@ document.addEventListener("DOMContentLoaded", (e) => {
       .then((response) => response.json())
       .then((data) => {
         articles = data;
-        // console.log(articles, "this is new articles data");
         searchCol();
         renderArticle(0);
-        // loadNewArticle();
-        
       });
   };
+
+  let testFunc = (e) => {
+    if(e.target.matches(".articleTitle")){
+      let id = parseInt(e.target.getAttribute("data-id")) - 1;
+      renderArticle(id)
+    }
+  };
+
+  //adding titles to search col
+  const searchCol = () => {
+    for (let i = 0; i < articles.length; i++) {
+      // console.log(articles[i].title);
+      let listItem = document.createElement("li");
+      let ArticleTitle = articles[i].title;
+      listItem.classList.add("articleTitle");
+      listItem.setAttribute("data-id", `${articles[i].id}`)
+      listItem.innerHTML = ArticleTitle;
+      articleTitleList.appendChild(listItem);
+    }
+    // console.log(listItem)
+  };
+  
+  const renderArticle = (id) => {
+    articleID = parseInt(id);
+    let title = articles[id].title
+    let content = articles[id].content
+    htmlTitle.innerHTML = title;
+    articleContent.innerHTML = content;
+  };
+
   //running this function allows the use of the data
   //from the table right away in other functions
   getArticles();
 
 articleTitleList.addEventListener("click", testFunc)
-  
 });
